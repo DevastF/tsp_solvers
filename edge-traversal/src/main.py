d6d718104e4b93a9e893cf9cf676baf5737c0b9a
Your Name <you@example.com>
2020-06-30 13:05:10 +0200

    python stuff

--- edge-traversal/src/main.py
+++ edge-traversal/src/main.py
@@ -3,33 +3,59 @@
 import numpy as np
 import pdb
 import collections
+from graph_tool.all import *
 from queue import PriorityQueue
 
 V = np.load("vertices.txt")
 nV = V.shape[0]
 nP = nV-1
 
-Mm = np.zeros((nV,nV))
-Mf = np.zeros((nV,nV))
+maxCost = 150
 
-Mf[0][3] = 1;
+g = Graph(directed=False);
 
-for i in range(0, nP):
-    Mf[1+i*(nP-1)][nP-1+i*(nP-1)]=20
-    Mf[1+i][1+2*(i+1)]=40
-    Mm[1+i][1+2*(i+1)]=80
+e_price = g.new_edge_property("int")
+v_coords = g.new_vertex_property("vector<double>")
+e_mandatory = [];
 
-graph = {};
-
-graph.
-
-def findNearestMandatoryEdge(vertex):
-   q = PriorityQueue()
-
-   
+goal_v = None
+goal_candidate_v = None
 
 
-def main():
+def initGraph():
+    for c in V:
+        v = g.add_vertex
+        v_coords[v] = c
+
+    for it in range(0,nP*2-2):
+        e = g.add_edge(g.vertex(1+it*2),g.vertex(2+it*2))        
+        e_price[e] = 20
+        e = g.add_edge(g.vertex(1+it*2),g.vertex(1+(it+1)*2))        
+        e_price[e] = 40
+        e = g.add_edge(g.vertex(1+it*2),g.vertex(1+(it+1)*2))        
+        e_price[e] = 80
+        e_mandatory.append(e);
+        e = g.add_edge(g.vertex(2+it*2),g.vertex(2+(it+1)*2))        
+        e_price[e] = 40
+        e = g.add_edge(g.vertex(2+it*2),g.vertex(2+(it+1)*2))        
+        e_price[e] = 80
+        e_mandatory.append(e);
+
+def nearestMandatoryEdge(vertice):
+
+
+
+class VisitorExample(gt.DijkstraVisitor):
+    def examine_vertex(self, v):
+        global goal_candidate_v
+        goal_candidate_v = v
+    def examine_edge(self, e):
+        global goal_v
+        if e_price[e] = 80
+            goal_v = goal_candidate_v          
+            
+
+ main():
     print("BOHICA")
 
 if __name__ == "__main__":
